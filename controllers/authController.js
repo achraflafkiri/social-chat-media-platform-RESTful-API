258bc3b738895939fcb4df2a36d88f8335ab9501
achraf LAFKIRI <lafkiri.achraf1@ofppt-edu.ma>
2023-02-11 21:52:53 +0100

    fix signin errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,10 +6,10 @@ const bcrypt = require("bcrypt");
 
 // GET PASSPORT
 const passport = async (user, res) => {
-  const token = jwt.sign({ id: user._id }, process.env.JWT, {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWTEXPIRESDELAI,
   });
-  console.log(token);
+  console.log("token => ", token);
   res.status(200).json({
     status: "success",
     user,
@@ -34,25 +34,24 @@ const Signup = catchAsync(async (req, res, next) => {
 });
 
 // SIGN IN
+
 const Signin = catchAsync(async (req, res, next) => {
-  const { email, username, password } = req.body;
-  if (!email && !username)
-    return next(new AppError(500, "please enter your email or username"));
-  if (!password)
-    return next(new AppError(500, "Your password is not correct!"));
-  if (email && password) {
-    const user = await User.findOne({ email: email });
-    if (!user) return next(new AppError(404, "You don't have an account"));
-    if (!user || !(await user.isCorrectPassword(password, user.password)))
-      return next(new AppError(401, "incorrect email or password"));
-    if (user) passport(user, res);
-  } else if (username && password) {
-    const user = await User.findOne({ username: username });
-    if (!user) return next(new AppError(404, "You don't have an account"));
-    if (!user || !(await user.isCorrectPassword(password, user.password)))
-      return next(new AppError(401, "incorrect email or password"));
-    if (user) passport(user, res);
-  }
+  console.log("**********Signin***********");
+  const { username, password } = req.body;
+  if (!username || !password)
+    return next(new AppError(404, "Username and Password are required"));
+
+  const user = await User.findOne({ username: username });
+  if (!user) return next(new AppError(401, "Invalid credentials"));
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return next(new AppError(401, "Invalid credentials"));
+
+  // send response to the client
+  res.status(200).json({
+    status: "success",
+    user: user,
+  });
 });
 
 module.exports = { Signup, Signin };
