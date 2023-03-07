af736b8a2285943691ddbb576195ecc0e3f281e4
achraf lafkiri <lafkiri.achraf1@ofppt-edu.ma>
2023-03-07 23:32:44 +0100

    added likes

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -1,12 +1,51 @@
-// const User = require("../models/UserModel");
-// const catchAsync = require("../utils/catchAsync");
-// const AppError = require("../utils/AppError");
-// const Like = require("../models/LikeModel");
+const File = require("../models/fileModel")
+const Like = require("../models/likesModel")
+const AppError = require("../utils/AppError")
+const catchAsync = require("../utils/catchAsync")
 
-// const postNewLike = catchAsync(async (req, res, next) => {
-//   console.log("************** post New Like *************");
-// });
+const checkLike= async(req,res)=>{
+    const object_ = await Like.findOne({user:req.user._id,file:req.params.musicId})
+    console.log(object_)
+    return object_!=null
+}
+const getAllLikeFormusicId = catchAsync(async(req,res,next)=>{
+    const likes = await Like.find({file:req.params.musicId})
+    res.status(200).json({
+        status : "success",
+        likes
+    })
+})
+const getOneLikeFormusicId = catchAsync(async(req,res,next)=>{
+    const like = await Like.find({file:req.params.likeId,_id:req.params.likeId})
+    res.status(200).json({
+        status : "success",
+        like
+    })
+})
+const postNewLike = catchAsync(async(req,res,next)=>{
+    const user = req.user._id
+     
+    const file =await File.findById(req.params.musicId)
+    if(!file) return next(new AppError(400,"fo file with this id"))
+    const newLike =await Like.create({file,user})
+    console.log(newLike)
+    res.status(200).json({
+        status : "success",
+        newLike
+    })
+    next()
+})
 
-// module.exports = {
-//   postNewLike,
-// };
+const deleteLike = catchAsync(async(req,res,next)=>{
+    // get the like 
+   /*  const like = await Like.findOne({user:req.user._id,file:req.params.musicId}) */
+    if(!await checkLike(req,res)) return next(new AppError(400,"you have no like on this file"))
+    await Like.findOneAndDelete({user:req.user._id,file:req.params.musicId})
+     res.status(200).send("deleted")
+})
+const deleteAllLikes = async(req,res,next)=>{
+    await Like.deleteMany()
+    res.send("all likes deleted")
+}
+
+module.exports={getAllLikeFormusicId,getOneLikeFormusicId,postNewLike,deleteLike,deleteAllLikes}
--- models/LikeModel.js
+++ models/LikeModel.js
@@ -1,15 +1,40 @@
-// const { mongoose, model, Schema } = require("mongoose");
+const { Schema, default: mongoose, model } = require("mongoose");
+const File = require("./fileModel");
 
-// const likesmodel = new Schema({
-//   user: {
-//     type: mongoose.Schema.ObjectId,
-//     ref: "User",
-//   },
-//   product: {
-//     type: mongoose.Schema.ObjectId,
-//     ref: "Product",
-//   },
-// });
+const likesSchema = new Schema({
+    user : {
+        type: mongoose.Schema.ObjectId,
+        ref:'User'
+    },file:{
+        type : mongoose.Schema.ObjectId,
+        ref:"File"
+    }
+})
+likesSchema.index({user:1,file:1},{unique:true})
 
-// const Like = model("Like", likesmodel);
-// module.exports = Like;
+likesSchema.statics.calcul=async function(musicId){
+    const stat =await this.aggregate(
+       [ {
+            $match : {file:musicId}
+        },{
+            $group:{
+                _id:'$file',
+                nLikes : {$sum : 1 } 
+            }
+        }]
+    )
+        if(stat.length ==0) return await File.findOneAndUpdate({_id:musicId},{likes:0})
+        await File.findOneAndUpdate({_id:musicId},{likes:stat[0].nLikes})
+}
+likesSchema.post("save",async function(doc){
+    console.log(doc.file._id,"*******§§§§§§§§§§§!!!")
+     doc.constructor.calcul(doc.file._id)
+})
+
+likesSchema.post('findOneAndDelete',async function(doc){
+    console.log(doc.file)
+
+    if(doc)return await doc.constructor.calcul(doc.file)
+})
+const Like = model("Like",likesSchema)
+module.exports=Like
